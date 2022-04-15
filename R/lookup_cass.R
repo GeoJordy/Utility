9b8b55150c09accf56550bfc3f11ec39d679530f
GeoJordy <Jordy.Bernard@geosyntec.com>
2022-04-14 17:11:34 -0800

    updating lookup_cass

--- R/lookup_cass.R
+++ R/lookup_cass.R
@@ -3,13 +3,14 @@
 #' @return if the string is associated with a cass number in the database, the cass number will be returned; if the string is not associated with a cass number, NA will be returned 
 #' @export
 lookup_cass <- function(search_string){
+  require(httr)
   url <- paste0(
     "https://commonchemistry.cas.org/api/search?q=",
     search_string
   )
   r <- httr::GET(url)
   if (r$status_code == 200){
-    a <- content(r, "parsed")
+    a <- httr::content(r, "parsed")
     if (length(a$results)==1){
       cas <- a$result[[1]]$rn
     }
